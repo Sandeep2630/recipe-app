35ad9a78129820635c12dbd59bd412e43271437e
sandeep2630 <sandeepthakur.delta4infotech@gmail.com>
2023-03-21 10:23:32 +0530

    did the client setup with react router dom and all other libraries

---
+++ client/src/pages/createRecipe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function createRecipe() {
+  return (
+    <div>
+      create-recipe
+    </div>
+  )
+}
---
+++ client/src/pages/savedRecipe.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function savedRecipe() {
+  return <div>saved recipe</div>;
+}
